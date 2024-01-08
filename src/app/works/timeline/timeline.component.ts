e7ad913689b3ae2dddb47869e12c6b31bc04d528
Gio1995 <37313582+Gio1995@users.noreply.github.com>
2024-01-08 21:28:31 +0100

    Changed description for Quid company

--- src/app/works/timeline/timeline.component.ts
+++ src/app/works/timeline/timeline.component.ts
@@ -9,7 +9,7 @@ import { Experience } from './experience';
 export class TimelineComponent implements OnInit {
 
   experience: Experience[] = [
-    {date: 'Jan 2024', company: 'Quid', logo: '../../../assets/images/companies/quid.jpeg', description: 'Angular Frontend Development.'},
+    {date: 'Jan 2024', company: 'Quid', logo: '../../../assets/images/companies/quid.jpeg', description: 'Backend development in Spring, Frontend development in Angular.'},
     {date: 'Oct 2022', company: 'Widiba', logo: '../../../assets/images/companies/widiba.png', description: 'Backend development in Struts, implementation of Rest services and use of PostgreSQL.'},
     {date: 'Jun 2022', company: 'Unicredit', logo: '../../../assets/images/companies/unicredit.png', description: 'Development of a Selenium-based framework for managing automated tests.'},
     {date: 'May 2022', company: 'Advancia-Tech', logo: '../../../assets/images/companies/advancia.jpeg', description: 'Training the fundamental concepts of web services and learning the Spring framework.'},
